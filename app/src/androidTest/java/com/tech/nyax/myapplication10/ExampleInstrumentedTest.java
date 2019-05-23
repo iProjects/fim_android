d1985d87b60a699fda9ecc90751f0a203f270d9f
kevin mutugi <kevinmk40@gmail.com>
2019-05-24 02:17:33 +0300

    started working with junit test

--- app/src/androidTest/java/com/tech/nyax/myapplication10/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/tech/nyax/myapplication10/ExampleInstrumentedTest.java
@@ -24,3 +24,19 @@ public class ExampleInstrumentedTest {
         assertEquals("com.tech.nyax.myapplication10", appContext.getPackageName());
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/test/java/com/tech/nyax/myapplication10/ExampleUnitTest.java
+++ app/src/test/java/com/tech/nyax/myapplication10/ExampleUnitTest.java
@@ -14,4 +14,9 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+    @Test
+    public void test_if_connected_to_server() {
+        assertEquals(4, 2 + 2);
+    }
+
 }
